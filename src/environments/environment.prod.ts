c3a0b77c01bd6c9bde0108674c9eef7576547afb
sharath-98 <sharathchander.p@gmail.com>
2023-06-02 11:17:16 -0500

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'pwd-manager-9dd6a',
+    appId: '1:925320378915:web:ea997358a5df6e577d0f96',
+    storageBucket: 'pwd-manager-9dd6a.appspot.com',
+    apiKey: 'AIzaSyAA0gLpxcnmBOjmWT0PUtokSnqAPymdN3g',
+    authDomain: 'pwd-manager-9dd6a.firebaseapp.com',
+    messagingSenderId: '925320378915',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pwd-manager-9dd6a',
+    appId: '1:925320378915:web:ea997358a5df6e577d0f96',
+    storageBucket: 'pwd-manager-9dd6a.appspot.com',
+    apiKey: 'AIzaSyAA0gLpxcnmBOjmWT0PUtokSnqAPymdN3g',
+    authDomain: 'pwd-manager-9dd6a.firebaseapp.com',
+    messagingSenderId: '925320378915',
+  },
   production: false
 };
 
