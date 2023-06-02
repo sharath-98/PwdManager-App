875b681ed2cadc734312ebc092047977658ac454
sharath-98 <sharathchander.p@gmail.com>
2023-06-02 16:13:30 -0500

    form update site data

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -11,23 +11,55 @@ export class SiteListComponent implements OnInit {
 
   allSites! :Observable<Array<any>>;
 
+  siteName !: string;
+  siteImgUrl !: string;
+  siteUrl !:string;
+  siteId !:string;
+  isEditEnabled: boolean = false;
+
   constructor(private pwdService: PasswordManagerService) { }
 
   ngOnInit(): void {
     this.loadSites();
   }
 
+  editSite(site: any){
+    this.siteName = site.siteName;
+    this.siteImgUrl = site.siteImgUrl;
+    this.siteId = site.id;
+    this.siteUrl = site.siteUrl;
+    this.isEditEnabled = true;
+
+  }
+
   onSubmit(values: object){
     console.log(values);
-    this.pwdService.addSite(values).then(()=>{
-      console.log("New Site Added.")
-    }).catch((err)=>{
-      console.log(err);
-    });
+    if(this.isEditEnabled){
+      this.pwdService.updateSite(this.siteId, values).then(()=>{
+        console.log('Data Updated.');
+      }).catch((err)=>{
+        console.log(err);
+      })
+    } else{
+      this.pwdService.addSite(values).then(()=>{
+            console.log("New Site Added.")
+          }).catch((err)=>{
+            console.log(err);
+          });
+    }
+    this.formReset();
   }
 
   loadSites(){
     this.allSites = this.pwdService.loadSites();
+  }
+
+  formReset(){
+    this.siteName = "";
+    this.siteImgUrl = "";
+    this.siteId = "";
+    this.siteUrl = "";
+    this.isEditEnabled = false;
   }
 
 }
