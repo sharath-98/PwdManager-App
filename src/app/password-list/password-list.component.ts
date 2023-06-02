bae0128bd682f80ceecabc1ab8babd807e03a499
sharath-98 <sharathchander.p@gmail.com>
2023-06-02 11:30:20 -0500

    navbar design

---
+++ src/app/password-list/password-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-list',
+  templateUrl: './password-list.component.html',
+  styleUrls: ['./password-list.component.css']
+})
+export class PasswordListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
